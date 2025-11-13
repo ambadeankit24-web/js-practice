468068ef32b18f30987d07d616ef37ad4cf007a8
ambadeankit24-web <ambadeankit24@gmail.com>
2025-11-13 17:26:36 +0000

    01_basics

---
+++ 01_basics/variable1.js
@@ -0,0 +1,11 @@
+const accountId = 12556;
+let accountEmail = "ankit@gmail.com";
+var accountPassword = 12345;
+
+accountCity = "Jaipur";
+
+accountEmail = "ambade@gmail.com";
+accountPassword = 25412;
+accountCity = "bengulure";
+
+console.table([accountEmail,accountId,accountCity,accountPassword]);
